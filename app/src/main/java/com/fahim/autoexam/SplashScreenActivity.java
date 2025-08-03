7af9a49d8213d293a1cd3b808365df202a0a72a4
ifahimkhan <khan.fahim95@hotmail.com>
2025-08-03 12:44:58 +0530

    if applied this commit will add SplashScreenActivity

---
+++ app/src/main/java/com/fahim/autoexam/SplashScreenActivity.java
@@ -0,0 +1,45 @@
+package com.fahim.autoexam;
+
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.fahim.autoexam.databinding.ActivityMainBinding;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Animate progress from 0 to 100 over 3 seconds
+        ObjectAnimator progressAnimator = ObjectAnimator.ofInt(binding.progressBar, "progress", 0, 100);
+        progressAnimator.setDuration(3000); // 3 seconds
+        progressAnimator.start();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, HomeActivity.class));
+                finish();
+            }
+        }, 3000);
+
+
+    }
+}
